82e39cb5a0ea5eb43d5d21ad85fbaad9971c3255
Ryan <ryanpvoong@gmail.com>
2024-07-12 18:11:10 -0400

    listed queries for postgres

--- app.py
+++ app.py
@@ -4,6 +4,23 @@ from psycopg2 import pool
 from flask import Flask
 from dotenv import load_dotenv
 
+CREATE_ROOMS_TABLE = (
+  "CREATE TABLE IF NOT EXISTS rooms (id SERIAL PRIMARY KEY, name TEXT);"
+)
+CREATE_TEMPS_TABLE = """CREATE TABLE IF NOT EXISTS temperatures (room_id INTEGER, temperature REAL,
+                        date TIMESTAMP, FOREIGN KEY(room_id) REFERENCES rooms(id) ON DELETE CASCADE);"""
+
+INSERT_ROOM_RETURN_ID = "INSERT INTO rooms (name) VALUES (%s) RETURNING id;"
+INSERT_TEMP = (
+  "INSERT INTO temperatures (room_id, temperature, date) VALUES (%s, %s, %s);"
+)
+
+# Get amount of different dates
+GLOBAL_NUMBER_OF_DAYS = (
+  """SELECT COUNT(DISTINCT DATE(date)) AS days FROM temperatures;"""
+)
+GLOBAL_AVG = """SELECT AVG(temperature) as average FROM temperatures;"""
+
 load_dotenv()
 
 app = Flask(__name__)
